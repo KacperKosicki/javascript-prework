0ddc765374b5c62826d729d86f90d38978f21b9f
Kacper Kosicki <kosickikacper1@gmail.com>
2023-05-09 20:40:10 +0200

    Add a player read

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if(randomNumber == 1) {
   computerMove = 'kamień';
 } else if(randomNumber == 2) {
 	computerMove = 'papier'
@@ -22,4 +22,20 @@ if(randomNumber == 1){
 	computerMove = 'nożyce'
 }
 
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Ruch komputera: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1') {
+  playerMove = 'kamień';
+} else if(playerInput == 2) {
+	playerMove = 'papier'
+} else if(playerInput == 3) {
+	playerMove = 'nożyce'
+}
+
+printMessage('Ruch gracza: ' + playerMove);
