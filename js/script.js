098a4f535b32137bc3513b3d34ff0ebd23bd1f0f
Kacper Kosicki <kosickikacper1@gmail.com>
2023-05-18 18:33:56 +0200

    Change let to const in computer and player movements

--- js/script.js
+++ js/script.js
@@ -19,31 +19,13 @@ function playGame(playerInput) {
 
   console.log('Wylosowana liczba to: ' + randomNumber);
 
-  let argComputerMove = getMoveName(randomNumber);
-
-  /*if(randomNumber == 1) {
-    computerMove = 'kamień';
-  } else if(randomNumber == 2) {
-    computerMove = 'papier'
-  } else if(randomNumber == 3) {
-    computerMove = 'nożyce'
-  }*/
+  const argComputerMove = getMoveName(randomNumber);
 
   printMessage('Ruch komputera: ' + argComputerMove);
 
-  //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
   console.log('Gracz wpisał: ' + playerInput);
 
-  let argPlayerMove = getMoveName(playerInput);
-
-  /*if(playerInput == '1') {
-    playerMove = 'kamień';
-  } else if(playerInput == 2) {
-    playerMove = 'papier'
-  } else if(playerInput == 3) {
-    playerMove = 'nożyce'
-  }*/
+  const argPlayerMove = getMoveName(playerInput);
 
   printMessage('Ruch gracza: ' + argPlayerMove);
 
@@ -56,7 +38,7 @@ function playGame(playerInput) {
       printMessage('Ty wygrywasz!');
     } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
       printMessage('Ty wygrywasz!');
-    } else if (argComputerMove = argPlayerMove) {
+    } else if (argComputerMove == argPlayerMove) {
       printMessage('Remis!');
     } else {
       printMessage('Tym razem przegrywasz :(');
