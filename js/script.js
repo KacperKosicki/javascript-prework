be26505a28e4ff00353b25a6b14e823e258c1814
Kacper Kosicki <kosickikacper1@gmail.com>
2023-05-20 10:53:41 +0200

    Replace a named function with an anonymous function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
  {
- function playGame(playerInput) {
+  const playGame = function(playerInput) {
   clearMessages();
-  function getMoveName(argMoveId){
+  const getMoveName = function(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
     }
@@ -30,7 +30,7 @@
 
   printMessage('Ruch gracza: ' + argPlayerMove);
 
-  function displayResult(argComputerMove, argPlayerMove){
+  const displayResult = function(argComputerMove, argPlayerMove){
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 
     if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
